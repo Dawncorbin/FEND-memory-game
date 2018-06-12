2afc2d96979f1f8cb8935ffa48eb905fe8b5226d
Dawn <imadesigner@hotmail.com>
2018-06-12 12:55:09 -0700

    Dynamically generated cards

--- js/app.js
+++ js/app.js
@@ -52,6 +52,18 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+  function initGame() {
+      let deck = document.querySelector ('deck');
+      let cardHTML = shuffle(cards).map (function(card) {
+          return generateCard(card);
+      });
+
+        deck.innerHTML = cardHTML.join('');
+  }
+
+  initGame();
+
  //open-show 2 cards at a time - if no match, they flip back over
   let allCards = document.querySelectorAll('.card');
   let openCards = [];
