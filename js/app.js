3e485bf1a8cb0c0b682b3a012bb377c76503a7f4
Dawn <imadesigner@hotmail.com>
2018-06-21 13:20:29 -0700

    Clean up code/new shuffle

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
+    const card = document.getElementsByClassName('card');
     const cards = ['fa-diamond', 'fa-diamond',
               'fa-paper-plane-o', 'fa-paper-plane-o',
               'fa-anchor', 'fa-anchor',
@@ -10,10 +11,8 @@
               'fa-bicycle', 'fa-bicycle',
               'fa-bomb', 'fa-bomb',
             ];
-//progamatically generate card HTML
-  function generateCard(card) {
-      return `<li class = "card" data-card= "${card}"><i class = "fa ${card}"></i></li>`;
-  }
+
+    const deck = document.querySelector('.deck');
 
   //loop to add event listeners to each card
 
@@ -23,9 +22,18 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
+//referenced this shuffle function by https://css-tricks.com/snippets/javascript/shuffle-array
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
+function shuffleDeck(o) {
+      for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+      return o;
+};
+
+cards.sort(function () {
+      return 0.25 - Math.random()
+});
+
+/*function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
@@ -38,7 +46,85 @@ function shuffle(array) {
 
     return array;
 }
+*/
 
+//create allCards
+  function generateCard() {
+    for (let i =o; i < cards.length; i++) {
+        let list = document.createElement("li");
+        deck.appendChild(list);
+        list.className = "card";
+
+        let info = document.createElement("i");
+
+        list.appendChild(info);
+        infor.className = `fa ${cards[i]} "data-set=" ${cards}`;
+    }
+  }
+
+generateCard();
+
+//shuffle new cards
+shuffleDeck(cards);
+
+//the following was helped along courtesy of Mike Wales webinar
+
+//compare mates
+let allCards = document.querySelectorAll('.card');
+let openCards = [];
+let matched = [];
+
+  allCards.forEach(function (card) {
+      card.addEventListener('click', function (e) {
+          if(!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
+              openCards.push(card);
+              card.classList.add('open', 'show');
+                if (openCards.length == 2) {
+
+                  //if a match
+                  if (openCards[0].innerHTML === openCards[1].innerHTML) {
+
+                    openCards[0].classList.add('match');
+                    openCards[0].classList.add('open');
+                    openCards[0].classList.add('show');
+
+                    openCards[1].classList.add('match');
+                    openCards[1].classList.add('open');
+                    openCards[1].classList.add('show');
+
+                    openCards = [];
+
+                    //if no match
+                  }else{
+                      setTimeout(function (openCards) {
+                          allCards.forEach(function (card) {
+                            card.classList.remove('open', 'show');
+
+                          });
+                      }, 1000);
+
+                      openCards = [];
+                      moves += 1;
+                      moveCounter.innerHTML = moves;
+
+                  }
+                  startCounter();
+                }
+                gameOver();
+
+          }
+      });
+  });
+
+//shuffles deck
+  /*function initGame() {
+      let deck = document.querySelector ('.deck');
+      let cardHTML = shuffle(cards).map (function(card) {
+          return generateCard(card);
+      });
+
+        deck.innerHTML = cardHTML.join('');
+  }*/
 
 /*
  * set up the event listener for a card. If a card is clicked:*/
@@ -53,74 +139,29 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-//shuffles deck
-  function initGame() {
-      let deck = document.querySelector ('.deck');
-      let cardHTML = shuffle(cards).map (function(card) {
-          return generateCard(card);
-      });
+//removes first and second stars based on move count after 12 & 16 Moves
+  function starRating () {
+      if (moves > 12 && moves < 16) {
+        first.style.visibility = 'hidden';
 
-        deck.innerHTML = cardHTML.join('');
+      } else if (moves > 16) {
+        second.style.visibility = 'hidden';
+      }
   }
 
-  initGame();
-
- //open-show 2 cards at a time - if no match, they flip back over
-  let allCards = document.querySelectorAll('.card');
-  let openCards = [];
-
-  allCards.forEach(function (card) {
-      card.addEventListener('click', function (e) {
-
-        if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
-          openCards.push (card);
-          card.classList.add('open', 'show');
-
-          //if match
-          if(openCards.length == 2) {
-              if (openCards [0].dataset.card == openCards[1].dataset.card) {
-                  openCards [0].classList.add('match');
-                  openCards [0].classList.add('open');
-                  openCards [0].classList.add('show');
-
-                  openCards [1].classList.add('match');
-                  openCards [1].classList.add('open');
-                  openCards [1].classList.add('show');
-
-                  openCards = [];
-              }else{
-            //if no match, hide
-                    setTimeout (function () {
-                      openCards.forEach (function (card) {
-                        card.classList.remove ('open', 'show');
-                      });
-
-                    openCards = [];
-                  }, 1000);
-                }
-            }
-          }
-      });
-  });
-
-  //starts seconds and minutes timer
-    function startTimer() {
-
-        timeElapsed = window.setInterval(function () {
-          timer.innterHTML = minutes + " mins "+ seconds + " secs";
-              seconds++;
-                if(seconds == 60) {
-                  minutes++;
-                  seconds = 0;
-                }
-        }, 1000);
+//move counter
+  function moveCount() {
+      moves++;
+      moveCounter.innerHTML = moves;
+        starRating();
     }
 
-    function resetTimer() {
-
-      clearInterval(timeElapsed);
-        minutes = 0;
-        seconds = 0;
-        timer = document.querySelector('.timer');
-            timer.innerHTML = minutes + " mins "+ seconds + " secs";
-    }
+//click counter, starts timer after first 'play'
+  function clickCount() {
+      if (clicks === 0) {
+        startTimer();
+        clicks++;
+      } else {
+        clicks++;
+      }
+  }
