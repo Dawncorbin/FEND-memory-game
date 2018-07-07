758cdf00121d475d8f7443a64fea793b60e8d245
Dawn <imadesigner@hotmail.com>
2018-07-06 22:09:21 -0700

    Improving CSS and JS flow

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
-
-const restart = document.querySelector('.restart');
-let modal = document.querySelector('.modal-content');
-
-
-
 //array holding all cards
 let allCards = ['fa-diamond', 'fa-diamond',
             'fa-paper-plane-o', 'fa-paper-plane-o',
@@ -14,17 +8,20 @@ let allCards = ['fa-diamond', 'fa-diamond',
             'fa-bicycle', 'fa-bicycle',
             'fa-bomb', 'fa-bomb'];
 
-//moves variables
-var movesCounter = document.querySelector('.moves');
-var moves = 0;
+//restart variable
+const restart = document.querySelector('.restart');
+
+//moves counter
+let movesCounter = document.querySelector('.moves');
+let moves = 0;
 
 //keep track of open cards
-var openCards = [];
+let openCards = [];
 
 //load new game - Shuffle
 newGame();
 
-// Shuffle function from http://stackoverflow.com/a/245096
+// Shuffle function from http://stackoverflow.com/a/2450976
       function shuffle(array) {
         var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -39,50 +36,40 @@ newGame();
         return array;
       }
 
+  //the following was referenced from Mike Wales Youtube walkthrough - https://www.youtube.com/watch?v=_rUH-sEs68Y
   //loop through each card and create its HTML
-      function makeCard(card) {
+  //add HTML to generate cards
+
+      function generateCard(card) {
           return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
       }
 
       function newGame() {
-        var deck = document.querySelector('.deck');
-        var cardGrid = shuffle(allCards).map(function (card) {
-          return makeCard(card);
-        });
-        deck.innerHTML = cardGrid.join('');
-      }
+          let deck = document.querySelector('.deck');
+          let cardGrid = shuffle(allCards).map(function (card) {
+            return generateCard(card);
+          });
+          deck.innerHTML = cardGrid.join('');
+        }
 
       let movesMade = 0;
       let cardsClicked = 0;
 
-      function starRating() {
-        const stars = document.querySelectorAll('.fa-star');
-        const starsArray = Array.apply(null, stars);
-        if (movesMade === 0) {
-            starsArray.forEach(x => x.className = 'fa fa-star');
-        }
-        if(movesMade > 10 && movesMade <= 12) {
-            starsArray[2].className = 'fa fa-star hide';
-        }
-        if (movesMade > 28) {
-            starsArray[2].className = 'fa fa-star hide';
-            starsArray[1].className = 'fa fa-star hide';
-        }
-      }
 
+
+      //timer referenced from fellow FEND student Chris N
       //timer
       let sec = 0;
       let min = 0;
       let timer;
-      var started = false;
+      let started = false;
 
       function startTimer() {
           if (!started) {
             timer = setInterval(insertTime, 1000);
             started = true;
           }
-
-      }
+        }
 
       function stopTimer() {
           clearInterval(timer);
@@ -101,13 +88,15 @@ newGame();
                 min++;
                 sec = "00";
             }
+
             //display time
             document.querySelector('.timerOutput').innerHTML = "0" + min + ":" + sec;
       }
 
       //set up event listener for cards
+      //compare matches
 
-      var cardList = document.querySelectorAll('.card');
+      let cardList = document.querySelectorAll('.card');
       for (let card of cardList) {
           card.addEventListener('click', function () {
               startTimer()
@@ -137,6 +126,8 @@ newGame();
               }
           });
       }
+
+      //if a match
       let matchedCards = [];
 
       function cardMatch() {
@@ -144,17 +135,12 @@ newGame();
         openCards[1].classList.add('match', 'open', 'show');
         matchedCards.push(openCards[0]);
         matchedCards.push(openCards[1]);
-        congratulations();
+        gameWon();
 
         openCards = [];
       }
 
-      restart.addEventListener('click', reset);
-
-      function reset() {
-        location.reload();
-      }
-
+      //if not a match
       function notMatching() {
 
         setTimeout(function () {
@@ -167,37 +153,54 @@ newGame();
         starRating();
       }
 
+      //restart game
+      restart.addEventListener('click', reset);
 
-//modal
-function congratulations() {
+      function reset() {
+        location.reload();
+      }
+
+      //star rating
+      function starRating() {
+        const stars = document.querySelectorAll('.fa-star');
+        const starsArray = Array.apply(null, stars);
+        if (movesMade === 0) {
+            starsArray.forEach(x => x.className = 'fa fa-star');
+        }
+        if(movesMade > 10 && movesMade <= 12) {
+            starsArray[2].className = 'fa fa-star hide';
+        }
+        if (movesMade > 28) {
+            starsArray[2].className = 'fa fa-star hide';
+            starsArray[1].className = 'fa fa-star hide';
+        }
+      }
+
+//once all matches are found - the timer stops and a modal opens
+//pop up modal
+let modal = document.querySelector('.modal-content');
+
+function gameWon() {
   if (matchedCards.length === 16) {
     stopTimer();
 
-    var starRating = document.querySelector('.stars').innerHTML;
+    let starRating = document.querySelector('.stars').innerHTML;
     document.getElementById('total-moves').innerHTML = movesMade;
     document.getElementById('total-stars').innerHTML = starRating;
     document.getElementById('endTime').innerHTML = document.querySelector('.timerOutput').innerHTML;
-    var modal = document.getElementById('myModal');
+    let modal = document.getElementById('myModal');
     modal.style.display = "block";
   }
 }
 
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 span.onclick = function () {
   modal.style.display = "none";
 }
-
-
-
-/*
-
-
- * Create a list that holds all of your cards
- */
-
-
-
+/* ***in addition to noted assistance above, I also referenced live webinar walkthroughs with Mike Wales & Ryan Waite, memory game blog posts from Matthew Cranford
+(https://matthewcranford.com/memory-game-walkthrough-part-1-setup/), FEND project 2 slack channel, https://developer.mozilla.org/en-US/, https://www.w3schools.com/,
+and https://stackoverflow.com/ *** */
 
 
  /*  - display the card's symbol (put this functionality in another function that you call from this one)
